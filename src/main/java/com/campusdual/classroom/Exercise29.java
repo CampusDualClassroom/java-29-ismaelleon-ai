b429801d02e954b38f8464abd868438014ee6da2
Ismael León Cal <ileoncal@gmail.com>
2024-11-07 17:14:26 +0100

    Ejercicio 29 Done

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,18 @@
 package com.campusdual.classroom;
 
+import java.io.IOException;
+
 public class Exercise29 {
     public static void main(String[] args) {
+        try {
+            operation();
+        } catch (IOException e) {
+            System.out.println("Se ha capturado una IOException: ");
+            e.printStackTrace();
+        }
+    }
+    public static void operation() throws IOException {
 
+        throw new IOException("Error al realizar la operación.");
     }
 }
